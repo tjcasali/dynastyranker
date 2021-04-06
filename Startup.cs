f3e80e23771e40e18589db8dd23c718983b7a080
Tim Casali <tjcasali@gmail.com>
2021-04-06 19:55:33 -0400

    Test images as Copy Always

--- Startup.cs
+++ Startup.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -41,6 +43,13 @@ namespace DynastyRanker
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            //app.UseStaticFiles(new StaticFileOptions()
+            //{
+            //    FileProvider = new PhysicalFileProvider(
+            //    Path.Combine(Directory.GetCurrentDirectory(), @"Images")),
+            //    RequestPath = "/Images"
+            //});
 
             app.UseRouting();
 
